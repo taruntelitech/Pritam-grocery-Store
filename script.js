8abb06968a4752bb8ef7ce2a23e4b73f8ad792ac
taruntelitech <taruntelitech@gmail.com>
2025-08-02 18:47:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function openForm(productName) {
-    document.getElementById('productName').value = productName;
-    document.getElementById('orderForm').style.display = 'block';
-  }
-
-  function closeForm() {
-    document.getElementById('orderForm').style.display = 'none';
-  }
-
